24d1b5a8b9b0d47e139c90a0ecc2bee385ae451d
Emmanuel <82957750+emmyduruc@users.noreply.github.com>
2024-01-06 20:19:17 +0100

    Add task and category migration to db

--- db/db.go
+++ db/db.go
@@ -38,7 +38,7 @@ func ConnectDb() {
 	db.Logger = logger.Default.LogMode(logger.Info)
 
 	log.Println("running migrations")
-	db.AutoMigrate(&models.User{})
+	db.AutoMigrate(&models.User{}, &models.Category{}, &models.Task{})
 	log.Println("migrations completed")
 
 	DB = Dbinstance{
--- models/models.go
+++ models/models.go
@@ -8,4 +8,25 @@ type User struct {
 	LastName  string `json:"lastname" gorm:"text;not null;default:null"`
 	Email     string `json:"email" gorm:"text;not null;default:null"`
 	Password  string `json:"password" gorm:"text;not null;default:null"`
+	Tasks     []Task `json:"tasks" gorm:"foreignKey:UserID"`
+}
+
+type Category struct {
+	gorm.Model
+	Name  string `json:"name" gorm:"text;not null;default:null"`
+	Tag   string `json:"tag" gorm:"text;not null;default:null"`
+	Color string `json:"color" gorm:"text;not null;default:null"`
+	Icon  string `json:"icon" gorm:"text;not null;default:null"`
+	Tasks []Task `json:"tasks" gorm:"foreignKey:CategoryID"`
+}
+
+type Task struct {
+	gorm.Model
+	CategoryID  uint   `json:"category_id" gorm:"integer;not null;default:null"`
+	UserID      uint   `json:"user_id" gorm:"integer;not null;default:null"`
+	Title       string `json:"title" gorm:"text;not null;default:null"`
+	Description string `json:"description" gorm:"text;not null;default:null"`
+	Completed   bool   `json:"completed" gorm:"boolean;not null;default:false"`
+	Priority    int    `json:"priority" gorm:"integer;not null;default:null"`
+	Status      string `json:"status" gorm:"text;not null;default:null"`
 }
