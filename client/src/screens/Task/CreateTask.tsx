845a7513f928805b98f13d90330d7138b1768680
Emmanuel <82957750+emmyduruc@users.noreply.github.com>
2024-01-09 01:42:48 +0100

    Add back drop on modal

--- client/src/screens/Task/CreateTask.tsx
+++ client/src/screens/Task/CreateTask.tsx
@@ -1,6 +1,5 @@
 import { Modal, View } from "react-native";
 import { GradientLayout } from "../../component/Layout/GradientLayout";
-import { Text } from "../../component/Text/Text";
 import { translate } from "../../services/translation.service";
 import { Input } from "../../component/Inputs/TextInput";
 import { useFormik } from "formik";
@@ -25,27 +24,35 @@ export const CreateTask = () => {
   return (
     <GradientLayout>
       <View className="flex-1 items-center justify-center">
-        <Modal animationType="slide" transparent={true} visible={modalVisible}>
-          <View className="flex-1 items-center bg-red-500 justify-center">
-            <Input
-              titleLabel={translate("title")}
-              onChange={onChange}
-              name={"title"}
-              formikFieldName={"title"}
-              autoFocus={true}
-              formik={formik}
-              className="flex-1 bg-slate-100"
-              validationSchema={taskValidationSchema}
-            />
-
-            <Input
-              titleLabel={translate("description")}
-              onChange={onChange}
-              name={"description"}
-              formikFieldName={"description"}
-              formik={formik}
-              validationSchema={taskValidationSchema}
-            />
+        <Modal
+          animationType="slide"
+          transparent={true}
+          visible={modalVisible}
+          onDismiss={() => setModalVisible(false)}
+        >
+          <View className="flex-1 items-center justify-center">
+            <View className="h-20 w-full">
+              <Input
+                titleLabel={translate("title")}
+                onChange={onChange}
+                name={"title"}
+                formikFieldName={"title"}
+                autoFocus={true}
+                formik={formik}
+                className="flex-1"
+                validationSchema={taskValidationSchema}
+              />
+            </View>
+            <View className="h-20 w-full">
+              <Input
+                titleLabel={translate("description")}
+                onChange={onChange}
+                name={"description"}
+                formikFieldName={"description"}
+                formik={formik}
+                validationSchema={taskValidationSchema}
+              />
+            </View>
           </View>
         </Modal>
       </View>
